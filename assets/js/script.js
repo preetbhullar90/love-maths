39e476be3991ad8f881787baee26220a9615621a
preetbhullar90 <gurpreet.singhb90@gmail.com>
2021-09-24 18:29:53 +0000

    Add event listners for document loading and buttton click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//wait for the dom to finish loading before running the game
+//Get the button elements and add listners to them
+
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons=document.getElementsByTagName('button');
+    for (let button of buttons){
+  button.addEventListener('click',function(){
+      if(this.getAttribute('data-type')==='submit'){
+          alert('You clicked Submit');
+      }else{
+          let gameType=this.getAttribute('data-type');
+          alert(`you clicked ${gameType}`);
+      }
+  })
+    }
+
+})
+
 function runGame(){
 
 }
